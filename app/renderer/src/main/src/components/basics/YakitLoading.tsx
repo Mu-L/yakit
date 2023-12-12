a864b0e9f0c37e31e88378ab9432d983cabe2b35
Youngster_yj <907985037@qq.com>
2023-12-12 12:05:17 +0800

    打包替换

--- app/renderer/src/main/src/components/basics/YakitLoading.tsx
+++ app/renderer/src/main/src/components/basics/YakitLoading.tsx
@@ -15,9 +15,11 @@ import {InstallEngine, QuestionModal} from "../layout/update/InstallEngine"
 import classNames from "classnames"
 import styles from "./yakitLoading.module.scss"
 import {getReleaseEditionName, isCommunityEdition, isEnpriTrace, isEnpriTraceAgent} from "@/utils/envfile"
-import {DynamicStatusProps} from "@/store"
-import yakitSE from "@/assets/yakitSE.png"
-import yakitEE from "@/assets/yakitEE.png"
+import { RemoteLinkInfo } from "../layout/UILayout"
+import { DynamicStatusProps } from "@/store"
+import yakitSE from "@/assets/yakitSE.png";
+import yakitEE from "@/assets/yakitEE.png";
+import telecomLogoSmall from "@/assets/telecom_logo_small.png"
 
 const {ipcRenderer} = window.require("electron")
 
@@ -616,7 +618,7 @@ export const YakitLoading: React.FC<YakitLoadingProp> = (props) => {
                     {isEnpriTraceAgent() && (
                         <div className={styles["yakit-loading-icon-wrapper"]}>
                             <div className={styles["white-icon"]}>
-                                <img src={yakitSE} alt='暂无图片' />
+                            <img src={telecomLogoSmall} alt="暂无图片" />
                             </div>
                         </div>
                     )}
